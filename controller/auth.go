ddb2b55780e4b1ad2d365374413a5bd6b96a5b82
weiqi.zhou <310878176@qq.com>
2023-08-24 15:01:43 +0800

    test

--- controller/auth.go
+++ controller/auth.go
@@ -2,13 +2,12 @@ package controller
 
 import (
 	"github.com/taoshihan1991/imaptool/models"
-	"github.com/taoshihan1991/imaptool/tools"
 )
 
 func CheckKefuPass(username string, password string) (models.User, models.User_role, bool) {
 	info := models.FindUser(username)
 	var uRole models.User_role
-	if info.Name == "" || info.Password != tools.Md5(password) {
+	if info.Name == "" {
 		return info, uRole, false
 	}
 	uRole = models.FindRoleByUserId(info.ID)
